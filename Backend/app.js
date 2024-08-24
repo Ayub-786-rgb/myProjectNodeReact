e44686c6839650e8d254e2ab5c3c72b0d769f6eb
Ayub-786-rgb <mohammadayub6sep1998@gmail.com>
2024-08-25 00:25:47 +0530

    changes for using .env

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 const app = express();
 
+require('dotenv').config();
 connectDB();
 
 app.use(bodyParser.json());
--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); 
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/myblogdb', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); 
   }
 };
 
